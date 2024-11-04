01275756e4ed072bc31431a5fd47f17b8661fe3e
Liriel-Barros <00001091657701sp@al.educacao.sp.gov.br>
2024-11-04 12:00:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'história',
+    'oficialmente, quando terminou a segunda guerra mundial?',
+    'em 1945'
+)
+
+criaCartao(
+    'Técnologia e Robótica',
+    'Qual o comando que desfaz uma ação no VsCode?',
+    'Ctrl Z'
+)
+
+criaCartao(
+    'Empreendedorismo',
+    'uma definição de marketing?',
+    'a cara da empresa, ou seja as cores e divulgação de produtos, assim como novos desings'
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'qual foi o principal fator que fez o navio titanic afundar?',
+    'a rpitura do casco, ocasionada por um ice-berg'
+)
